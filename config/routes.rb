9c3a88841a31601d2d644687637ffa3646844a0d
KazumaUede <katsuodashi794@gmail.com>
2018-12-03 15:45:41 +0900

    7.1.2 Add route Users

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
 
 
   root 'static_pages#home'
-  get 'foo/bar'
-  get 'foo/baz'
   # 回りくどくなるので下のように名前を意義する。
   get   '/help', to: 'static_pages#help'
   get   '/about', to: 'static_pages#about'
   get   '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users
 end
