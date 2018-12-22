873ca35b9b2ea55a33131e99eb71e26f010ae2d2
KazumaUede <katsuodashi794@gmail.com>
2018-12-22 15:21:28 +0900

    User signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   # 回りくどくなるので下のように名前を意義する。
-  get   '/help', to: 'static_pages#help'
-  get   '/about', to: 'static_pages#about'
-  get   '/contact', to: 'static_pages#contact'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
 end
