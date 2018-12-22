ccbb9f25e45f535908eb8731a42c2569fe9c83a9
KazumaUede <katsuodashi794@gmail.com>
2018-12-22 18:31:07 +0900

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,5 +26,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
